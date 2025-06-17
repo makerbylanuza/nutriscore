65c048998c5763f7cc6c18605eefd94cba10cfd6
Maker by Lanuza <makerspace@juandelanuza.org>
2025-06-17 08:24:55 +0000

    cambios

--- src/main.py
+++ src/main.py
@@ -4,16 +4,26 @@ import pytesseract
 import os
 import re
 
-nombres_aceites = {
-    "aceite de palma": r"aceite de palma",
-    "aceite de girasol": r"aceite de girasol",
-    "aceite de colza": r"aceite de colza",
-    "aceite de soja": r"aceite de soja|aceite de soya",
-    "aceite de maíz": r"aceite de maíz",
-    "aceite de sésamo": r"aceite de sésamo",
+ingredientes_buscados = {
+    # Aceites de semilla
+    "aceite de palma": r"aceite de palma|aceite de palmiste|grasa de palma|grasa vegetal (palma)|grasa vegetal fraccionada e hidrogenada de palmiste|estearina de palma|palmoleina|oleina de palma|manteca de palma|elaeis guineensis",
+    "aceite de girasol": r"aceite de girasol|aceite de maravilla|aceite de semilla de girasol|helianthus annus",
+    "aceite de colza": r"aceite de colza|aceite de nabina|aceite de canola|brassica napus",
+    "aceite de soja": r"aceite de soja|aceite de soya|glycine soya oil|óleo de soja|óleo de soya",
+    "aceite de maíz": r"aceite de maíz|aceite de rgano de maíz|aceite de zea mays",
+    "aceite de sésamo": r"aceite de sésamo|aceite de ajonjolí|aceite de benina|nallennai",
+    
+    # Azúcares y edulcorantes
+    "jarabe de glucosa": r"jarabe de glucosa|jarabe de fructosa|jarabe de maíz|jarabe de glucosa y fructosa|jarabe de glucosa-fructosa|glucosa|fructosa|dextrosa|jarabe de maíz de alta fructosa",
+    "sucralosa": r"sucralosa|E955|E-955|E 955",
+    "sacarina": r"sacarina|E954|E-954|E 954",
+    "aspartamo": r"aspartamo|E951|E-951|E 951",
+    "acesulfamo-k": r"acesulfamo k|acesulfamo-k|ace k|ace-k|acesulfamo de potasio|acesulfame k|acesulfame-k|E950|E-950|E 950",
+    "ciclamato": r"ciclamato|ciclohexilsulfamato|sodium cyclamate|E952|E-952|E 952",
+    
 }
 
-contiene_aceites = {
+contiene_ingredientes = {
 
 }
 
@@ -52,14 +62,14 @@ if __name__ == "__main__":
     # Crea una imagen de prueba simple (o usa una existente)
     # Para probar, podrías tener una imagen .png o .jpg en tu repositorio
     # Por ejemplo, una imagen llamada 'test_image.png'
-    test_image_path = "tests/data/margarina.jpg"
+    test_image_path = input("imagen: ")
 
     # Ejecuta el OCR
     extracted_text = perform_ocr(test_image_path)
     print(extracted_text)
 
-    for aceite, patron in nombres_aceites.items():
+    for ingrediente, patron in ingredientes_buscados.items():
         if re.search(patron, extracted_text.lower()):
-            print(f"¡Se encontró '{aceite}' en los ingredientes!")
+            print(f"¡Se encontró '{ingrediente}' en los ingredientes!")
         else:
-            print(f"'{aceite}' no encontrado.")
+            print(f"'{ingrediente}' no encontrado.")
